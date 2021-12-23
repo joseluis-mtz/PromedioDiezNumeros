10474ad3bd23db6bee49293743b8badf07a03ab5
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-23 13:49:38 -0600

    Agregar código alternativo.

--- PromedioDiezNumeros/Program.cs
+++ PromedioDiezNumeros/Program.cs
@@ -23,6 +23,58 @@ namespace PromedioDiezNumeros
             }
             Console.WriteLine("\nEl promedio es: " + (promedio = calificacion / 10));
             Console.ReadKey();
+
+            #region Código alternativo NO RECOMENDADO (Comentar el código anterior para usar este)
+            //int calif1 = 0;
+            //int calif2 = 0;
+            //int calif3 = 0;
+            //int calif4 = 0;
+            //int calif5 = 0;
+            //int calif6 = 0;
+            //int calif7 = 0;
+            //int calif8 = 0;
+            //int calif9 = 0;
+            //int calif10 = 0;
+            //int Promedio = 0;
+            //int suma = 0;
+
+            //Console.Write("Escribe la calificación 1: ");
+            //calif1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 2: ");
+            //calif2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 3: ");
+            //calif3 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 4: ");
+            //calif4 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 5: ");
+            //calif5 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 6: ");
+            //calif6 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 7: ");
+            //calif7 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 8: ");
+            //calif8 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 9: ");
+            //calif9 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Escribe la calificación 10: ");
+            //calif10 = int.Parse(Console.ReadLine());
+
+            //suma = calif1 + calif2 + calif3 + calif4 + calif5 + calif6 + calif7 + calif8 + calif9 + calif10;
+            //Promedio = suma / 10;
+
+            //Console.WriteLine();
+            //Console.WriteLine("El promedio es: " + Promedio);
+            //Console.ReadKey();
+            #endregion
         }
     }
 }
