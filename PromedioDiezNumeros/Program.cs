feb5a72115ee11825bf1b1def6ffabd3df08b130
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-23 13:43:19 -0600

    Archivos iniciales y metodo guardar calificaciones.

---
+++ PromedioDiezNumeros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromedioDiezNumeros
+{
+    class Program
+    {
+        public static void guardarCalificacion(ref int calificacion, int valor)
+        {
+            calificacion += valor;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
